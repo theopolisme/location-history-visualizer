d2519e2bad3ec27d538424eb8a595b274e8ead02
theopolisme <theopolismewiki@gmail.com>
2014-10-05 01:45:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# location-history-visualizer
+
+**Available online: [theopolisme.github.io/location-history-visualizer]()**
+
+A tool for visualizing your complete, consolidated, collected Google [Location History](https://google.com/locationhistory).
+
+It works directly in your web browser &ndash; no software to download, no packages to install. **Everyone deserves to know what data is being collected about them, without having to fiddle with cryptic pieces of software.**
+
+*location-history-visualizer* takes raw Google Takeout output and produces a heatmap of all of your location data over time, overlaid on an interactive map.
+
+## Packages used
+* [leaflet.js](http://leafletjs.com/), for rendering the interactive map
+* [leaflet.heat](https://github.com/Leaflet/Leaflet.heat), for rendering the heatmap overlay
+* [filestream](https://github.com/DamonOehlman/filestream), for dealing with gigantic Google Takeout files
+* [oboe.js](http://oboejs.com), for processing said gigantic files
+* [browserify](http://browserify.org/), for helping filestream  work properly in the browser
+
+
+## License
+
+Copyright (C) 2014 Theopolisme <theopolismewiki@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ index.js
@@ -0,0 +1,108 @@
+( function ( $, L, oboe, FileReadStream ) {
+	var map;
+
+	// Start at the beginning
+	stageOne();
+
+	////// STAGE 1 - ZE VELCOME UNT ZE UPLOAD //////
+
+	function stageOne () {
+		var dropzone;
+
+		// Initialize the map
+		map = L.map( 'map' ).setView([0,0], 2);
+		L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: 'location-history-visualizer is open source and available <a href="https://github.com/theopolisme/location-history-visualizer">on GitHub</a>. Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors.',
+			maxZoom: 18
+		} ).addTo( map );
+
+		// Initialize the dropzone
+		dropzone = new Dropzone( document.body, {
+			url: '/',
+			previewsContainer: '#null',
+			clickable: false,
+			accept: function ( file, done ) {
+				if ( file.type !== 'application/json' ) {
+					alert( 'Whoops, make sure you upload a .json file.' );
+				} else {
+					stageTwo( file );
+					dropzone.disable(); // Your job is done, buddy
+				}
+			}
+		} );
+	}
+
+	////// STAGE 2 - ZE PROCESSING //////
+
+	function stageTwo ( file ) {
+		var heat = L.heatLayer( [], {
+				blur: 20
+ 			} ).addTo( map ),
+			SCALAR_E7 = 0.0000001; // Since Google Takeout stores latlngs as integers
+
+		// First, change tabs
+		$( 'body' ).addClass( 'working' );
+		$( '#intro' ).addClass( 'hidden' );
+		$( '#working' ).removeClass( 'hidden' );
+
+		// Now start working!
+		processFile( file );
+
+		function status ( message ) {
+			$( '#currentStatus' ).text( message );
+		}
+
+		function processFile ( file ) {
+			var pointNo = 0,
+				filestream = new FileReadStream( file );
+
+			status( 'Reading file...' );
+
+			oboe( filestream )
+				.on( 'node', {
+					'locations.*': function ( location ) {
+						// Add the new point... prevent lots of redraws by writing to _latlngs
+						pointNo += 1;
+						status( 'Adding point #' + pointNo.toLocaleString() + '...' );
+						heat._latlngs.push( [ location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7 ] );
+					},
+					'locations': function () {
+						// Don't need any other data now
+						this.abort();
+						// Also, trigger the next step :D
+						renderMap();
+					}
+				} )
+				.on( 'fail', function () {
+					status( 'Something went wrong reading your JSON file. Ensure you\'re uploading a "direct-from-Google" JSON file and try again, or create an issue on GitHub if the problem persists.' );
+   				} );
+
+			function renderMap () {
+				heat.redraw();
+				// Stage 3!
+				stageThree( /* numberProcessed */ pointNo );
+			}
+		}
+	}
+
+	////// STAGE 3 - THEY GROW UP SO FAST //////
+
+	function stageThree ( numberProcessed ) {
+		var $done = $( '#done' );
+
+		// Change tabs :D
+		$( 'body' ).removeClass( 'working' );
+		$( '#working' ).addClass( 'hidden' );
+		$done.removeClass( 'hidden' );
+
+		// Update count
+		$( '#numberProcessed' ).text( numberProcessed.toLocaleString() );
+
+		// Fade away when clicked
+		$done.one( 'click', function () {
+			$( 'body' ).addClass( 'map-active' );
+			$done.fadeOut();
+		} );
+	}
+
+}( jQuery, L, oboe, FileReadStream ) );
