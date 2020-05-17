252b1036f2898a46b8c8aa44bf6999b94aa4cce0
jmoura <operte@gmail.com>
2020-05-17 14:36:41 +0200

    remove heatmap controls

--- index.js
+++ index.js
@@ -117,53 +117,12 @@
 	        .always( function () {
 	          $( 'body' ).addClass( 'map-active' );
 	          $done.fadeOut();
-	          activateControls();
 	        } );
 	      } else {
 	        alert( 'Please enter a valid email address to proceed.' );
 	      }
 	    } );
 
-			function activateControls () {
-				var $tileLayer = $( '.leaflet-tile-pane' ),
-					$heatmapLayer = $( '.leaflet-heatmap-layer' ),
-					originalHeatOptions = $.extend( {}, heatOptions ); // for reset
-
-				// Update values of the dom elements
-				function updateInputs () {
-					var option;
-					for ( option in heatOptions ) {
-						if ( heatOptions.hasOwnProperty( option ) ) {
-							document.getElementById( option ).value = heatOptions[option];
-						}
-					}
-				}
-
-				updateInputs();
-
-				$( '.control' ).change( function () {
-					switch ( this.id ) {
-						case 'tileOpacity':
-							$tileLayer.css( 'opacity', this.value );
-							break;
-						case 'heatOpacity':
-							$heatmapLayer.css( 'opacity', this.value );
-							break;
-						default:
-							heatOptions[ this.id ] = Number( this.value );
-							heat.setOptions( heatOptions );
-							break;
-					}
-				} );
-
-				$( '#reset' ).click( function () {
-					$.extend( heatOptions, originalHeatOptions );
-					updateInputs();
-					// Reset opacity too
-					$heatmapLayer.css( 'opacity', originalHeatOptions.heatOpacity );
-					$tileLayer.css( 'opacity', originalHeatOptions.tileOpacity );
-				} );
-			}
 		}
 
 		/*
